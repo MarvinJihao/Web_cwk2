956c31c3b21b99e85948add9067f4f5dec624946
Hao_Ji <sc23hj@leeds.ac.uk>
2026-04-26 08:06:15 +0100

    test: cover crawler link discovery and politeness

---
+++ tests/test_crawler.py
@@ -0,0 +1,71 @@
+from src.crawler import Crawler
+
+
+class FakeResponse:
+    def __init__(self, text):
+        self.text = text
+
+    def raise_for_status(self):
+        return None
+
+
+class FakeSession:
+    def __init__(self, pages):
+        self.pages = pages
+        self.requested_urls = []
+
+    def get(self, url, timeout):
+        self.requested_urls.append(url)
+        return FakeResponse(self.pages[url])
+
+
+def test_crawler_collects_internal_pages_and_text():
+    session = FakeSession(
+        {
+            "https://quotes.toscrape.com": """
+                <html><body>
+                  <p>First quote</p>
+                  <a href="/page/2/">next</a>
+                  <a href="https://external.example/">external</a>
+                </body></html>
+            """,
+            "https://quotes.toscrape.com/page/2": "<html><body>Second quote</body></html>",
+        }
+    )
+
+    crawler = Crawler(
+        "https://quotes.toscrape.com/",
+        politeness_delay=0,
+        session=session,
+    )
+
+    pages = crawler.crawl()
+
+    assert [page.url for page in pages] == [
+        "https://quotes.toscrape.com",
+        "https://quotes.toscrape.com/page/2",
+    ]
+    assert "First quote" in pages[0].text
+    assert "Second quote" in pages[1].text
+
+
+def test_crawler_waits_between_successive_requests():
+    session = FakeSession(
+        {
+            "https://quotes.toscrape.com": '<a href="/page/2/">next</a>',
+            "https://quotes.toscrape.com/page/2": "Done",
+        }
+    )
+    sleeps = []
+
+    crawler = Crawler(
+        "https://quotes.toscrape.com/",
+        politeness_delay=6,
+        session=session,
+        sleeper=sleeps.append,
+    )
+
+    crawler.crawl()
+
+    assert sleeps == [6]
+
